8c0554f13bace4241b3879b2c73bceb57ed89f59
liudaifeng1 <38613651+liudaifeng1@users.noreply.github.com>
2018-04-27 00:46:42 +0800

    Add files via upload

---
+++ new_file10.php
@@ -0,0 +1,30 @@
+﻿<?php
+	//存在一个数据表DATA,存储用户表白的数据，有六个字段,第一个为表白时间TIME，第二个为表白内容CONTENT，第三个为表白图片IMG，第四个为
+	//表白歌曲MUSIC，第五个为表白视频VIDEO，第六个为表白文件FILE；
+$sdf=$_POST["sdf"];
+$tyu=$_POST["tyu"];
+$tyu=trim($tyu);
+$currentTime=$_POST["currentTime"];
+$music=$_POST["music"];
+$video=$_POST["video"];
+$wenjian=$_POST["wenj"];
+$host="sqld.duapp.com";
+$user="a1b8ac14cfe5464cae9876cee0d01ff4";
+$password="3a4fc2bb0dfb46bab31f721ef8748c60";
+$port="4050";
+$database="QUwcfWAWcAgyxlPUFUln";
+$conn=mysqli_connect($host, $user, $password, $database, $port);
+if(!$conn){
+echo "wrong";
+}
+else{
+	$insert="INSERT INTO DATA (TIME,CONTENT,IMG,MUSIC,VIDEO,FILE) VALUES ('".$currentTime."','".$tyu."','".$sdf."','".$music."','".$video."','".$wenjian."')";
+	$an2=mysqli_query($conn,$insert);
+	if($an2){
+		echo "success";
+	}
+	else{
+		echo "fail";
+	}
+}
+?>
---
+++ new_file11.php
@@ -0,0 +1,66 @@
+﻿<?php
+$jishu = -1;
+$host = "sqld.duapp.com";
+$user = "a1b8ac14cfe5464cae9876cee0d01ff4";
+$password = "3a4fc2bb0dfb46bab31f721ef8748c60";
+$port = "4050";
+$database = "QUwcfWAWcAgyxlPUFUln";
+$conn = mysqli_connect($host, $user, $password, $database, $port);
+if (!$conn) {
+	echo "wrong";
+} else {
+	$select = "SELECT * FROM DATA";
+	$an3 = mysqli_query($conn, $select);
+	$pb3 = mysqli_affected_rows($conn);
+	if ($pb3 < 0) {
+		echo "error";
+	}
+	if ($pb3 == 0) {
+		echo "null";
+	}
+	if ($pb3 > 0) {
+		$ay = array();
+		$by = array();
+		$cy = array();
+		$dy = array();
+		$ey = array();
+		$fy = array();
+		$result = array();
+		if($pb3>=20){
+			for ($io = $_POST["kt"]; $io < 20 * $_POST["counte"]; $io++) {
+			$jishu++;
+			$reo = mysqli_data_seek($an3, $io);
+			$fk = mysqli_fetch_row($an3);
+			$ay[$jishu] = $fk[0];
+			$by[$jishu] = $fk[1];
+			$cy[$jishu] = $fk[2];
+			$dy[$jishu] = $fk[3];
+			$ey[$jishu] = $fk[4];
+			$fy[$jishu] = $fk[5];
+		}
+		}
+		else{
+			for($io=0;$io<$pb3;$io++){
+				$jishu++;
+			$fk=mysqli_fetch_array($an3, MYSQLI_NUM);
+			$ay[$jishu] = $fk[0];
+			$by[$jishu] = $fk[1];
+			$cy[$jishu] = $fk[2];
+			$dy[$jishu] = $fk[3];
+			$ey[$jishu] = $fk[4];
+			$fy[$jishu] = $fk[5];
+			}
+		}
+		for ($iy = 0; $iy < count($ay); $iy++) {
+			$eri = array("time", "content", "img", "music", "video", "fil");
+			$df = array();
+			array_push($df, $ay[$iy], $by[$iy], $cy[$iy], $dy[$iy], $ey[$iy], $fy[$iy]);
+			$combine = array_combine($eri, $df);
+			$result[$iy] = $combine;
+		}
+		$yu = json_encode($result);
+		//转换后的格式为"[{},{},{}]"
+		echo $yu;
+	}
+}
+?>
